fdc7eeea7d28e069316eadbda92e5fbf6add3188
mlorenzm <mlorenzomohamed@gmail.com>
2023-11-14 18:51:53 +0000

    add untested ship class

---
+++ src/modules/Ship.js
@@ -0,0 +1,18 @@
+export default class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+  hit() {
+    if (this.hits < this.length) {
+      this.hits++;
+    }
+  }
+  isSunk() {
+    if (this.hits == this.length) {
+      this.sunk = true;
+      return this.sunk;
+    }
+  }
+}
