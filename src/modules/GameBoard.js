f69b5f3d9309e15dfe2275cc3c4d21b85a7d89cf
mlorenzm <mlorenzomohamed@gmail.com>
2023-11-17 19:29:47 +0000

    add createShipFleet tests

--- src/modules/GameBoard.js
+++ src/modules/GameBoard.js
@@ -1,4 +1,4 @@
-import Ship from Ship.js
+import Ship from "./Ship.js";
 export default class GameBoard {
   constructor() {
     this.board = this.createBoard();
@@ -14,12 +14,23 @@ export default class GameBoard {
     }
     return arr;
   }
-  placeShip(){
-
+  createShipFleet() {
+    const ships = [];
+    const sizes = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4];
+    for (let i = 0; i < sizes.length; i++) {
+      const ship = new Ship(sizes[i]);
+      ships.push(ship);
+    }
+    return ships;
   }
-  placementIsLegal(x,y){
-    if (0<=x<=10) && (0<=y<=10){
-      return true
+  placeShip(x, y) {
+    // x,y is the coordinate for the head of the ship. It places all ships vertically
+
+    const ship = new Ship(size);
+  }
+  placementIsLegal(x, y) {
+    if (0 <= x <= 10 && 0 <= y <= 10) {
+      return true;
     }
   }
 }
--- src/modules/tests/Gameboard.test.js
+++ src/modules/tests/Gameboard.test.js
@@ -1,4 +1,5 @@
 import GameBoard from "../GameBoard.js";
+import Ship from "../Ship.js";
 
 test("the gameboard.board is a 2d array of size 10 null", () => {
   const gameboard = new GameBoard();
@@ -24,4 +25,14 @@ test("the gameboard.board is a 2d array of size 10 null", () => {
     }
   }
   expect(gameboard.board).toStrictEqual(arr);
+});
+
+test("there is an array of ships created", () => {
+  const gameboard = new GameBoard();
+  const ships = [];
+  const sizes = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4];
+  for (let i = 0; i < sizes.length; i++) {
+    ships.push(new Ship(sizes[i]));
+  }
+  expect(gameboard.createShipFleet()).toStrictEqual(ships);
 });
