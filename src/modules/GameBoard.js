71a5281718dff0ade072ae222914d28574b58e34
mlorenzm <mlorenzomohamed@gmail.com>
2023-11-20 19:13:22 +0000

    fix createBoard method

--- src/modules/GameBoard.js
+++ src/modules/GameBoard.js
@@ -2,17 +2,19 @@ import Ship from "./Ship.js";
 export default class GameBoard {
   constructor() {
     this.board = this.createBoard();
+    this.ships = this.createShipFleet();
   }
   createBoard() {
-    let arr = [];
-    let size = 10;
-    for (let i = 0; i < size; i++) {
-      arr[i] = [];
-      for (let j = 0; j < size; j++) {
-        arr[i][j] = null;
+    let array = [];
+    let arrayItem = [];
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        arrayItem.push({ shipName: undefined, shipIndex: undefined });
       }
+      array.push(arrayItem);
+      arrayItem = [];
     }
-    return arr;
+    return array;
   }
   createShipFleet() {
     const ships = [];
@@ -23,14 +25,27 @@ export default class GameBoard {
     }
     return ships;
   }
-  placeShip(x, y) {
+  placeShip(x, y, ship) {
+    const len = ship.length;
     // x,y is the coordinate for the head of the ship. It places all ships vertically
-
-    const ship = new Ship(size);
+    if (this.placementIsLegal(x, y, len)) {
+      for (let i = 0; i < len; i++) {
+        this.gameBoardArray[y + i][x].shipName = ship;
+        this.gameBoardArray[y + i][x].shipIndex = i;
+      }
+    }
   }
-  placementIsLegal(x, y) {
-    if (0 <= x <= 10 && 0 <= y <= 10) {
+  placementIsLegal(x, y, len) {
+    if (x > 10 || x < 0 || y > 10 || y < 0 || y + len > 10) {
+      return false;
+    } else {
+      for (let i = y; i < y + len; i++) {
+        if (this.gameBoardArray[i][x].shipName != undefined) {
+          return false;
+        }
+      }
       return true;
     }
   }
+  receiveAttack(x, y) {}
 }
