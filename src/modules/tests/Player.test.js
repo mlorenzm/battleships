0d3c594a6b0894812afeb12a77d4380bb1b14072
mlorenzm <mlorenzomohamed@gmail.com>
2023-11-21 18:58:47 +0000

    moved player class to js file instead of test file

--- src/modules/tests/Player.test.js
+++ src/modules/tests/Player.test.js
@@ -1,16 +1,1 @@
-import GameBoard from "../GameBoard";
-import Ship from "../Ship";
-export default class Player {
-  constructor() {
-    this.turn = true;
-    this.board = GameBoard.board;
-  }
-  getTurn() {
-    return this.turn;
-  }
-  setTurn() {
-    if (!this.turn) {
-      this.turn = true;
-    }
-  }
-}
+
