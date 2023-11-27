7dff5763acc54f21ea5ab60cf9fff1b56d2eaa3e
mlorenzm <mlorenzomohamed@gmail.com>
2023-11-27 21:43:07 +0000

    UI version one

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import "./style.scss";
-import GameBoard from "./modules/GameBoard";
-import Ship from "./modules/Ship";
+
 import Player from "./modules/Player";
 import Computer from "./modules/Computer";
+
+const generateGrid = (x) => {
+  const element = document.getElementById(x);
+  for (let i = 0; i <= 9; i++) {
+    for (let j = 0; j <= 9; j++) {
+      const cell = document.createElement("div");
+      cell.dataset.x = i;
+      cell.dataset.y = j;
+      cell.addEventListener("click", () => {
+        return [parseInt(cell.dataset.x), parseInt(cell.dataset.y)];
+      });
+      element.appendChild(cell);
+    }
+  }
+};
+
+const gameLoop = () => {
+  const player = new Player();
+  const computer = new Computer();
+};
+
+generateGrid("player");
+generateGrid("computer");
